d09bfca20abfb17bfba012fbd32703c63fa308a5
devpigKing <devpigking@gmail.com>
2023-08-15 21:48:47 +0900

    #feat_1 : 블로그 컨트롤러 입니다.

---
+++ src/main/java/com/pigkinglog/myblog/controller/BlogApiController.java
@@ -0,0 +1,58 @@
+package com.pigkinglog.myblog.controller;
+
+import com.pigkinglog.myblog.domain.Article;
+import com.pigkinglog.myblog.dto.AddArticleRequest;
+import com.pigkinglog.myblog.dto.ArticleResponse;
+import com.pigkinglog.myblog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달 받은 URL와 동일하면 메서드로 매핑
+    @PostMapping("/api/articles") // 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) { // PathVariable은 URL에서 값을 가져오는 애너테이션
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+}
