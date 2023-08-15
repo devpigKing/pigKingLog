c9e4fa8c609fe337defd5910682cef59b2f1ad16
devpigKing <devpigking@gmail.com>
2023-08-16 01:25:14 +0900

    #feat_2 : 블로그 글 뷰 구현을 위한 엔티티 생성 및 sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (title, content) VALUES ('제목 1', '내용 1')
-INSERT INTO article (title, content) VALUES ('치즈볼 2', '치즈볼은 맛있다.')
-INSERT INTO article (title, content) VALUES ('제목 3', '내용 3')
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('돼지갈비찜', '돼지갈비찜은 맛있을까?', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('치즈볼 2', '치즈볼은 맛있다.', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 3', '내용 3', NOW(), NOW())
